7e923c5e4f4f4354ef8f1c3e00f88573a09826ea
mouhamed anflouss <mouhamedanflosse@gmail.com>
2023-12-19 18:28:38 +0100

    enhancement

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Count from "./component/Count";
 
 function App() {
-  const [counter,setCounter] = useState(0)
-
   return (
-  <>
-  <main className='flex flex-col item-center space-y-2 container mt-28 mx-auto w-60'>
-   <p className={`text-5xl font-bold w-full text-center ${counter > 0 ? "text-blue-500" : counter < 0 && "text-red-500" }`}>{counter}</p>
-   <div className='flex item-center w-full justify-center gap-4'>
-   <button onClick={() => setCounter((prevState) => prevState + 1 )} class="text-white  shadow-md transition-all bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">+</button>
-   <button onClick={() => setCounter((prevState) => prevState - 1 )} class="text-white bg-red-500 hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5">-</button>
-   </div>
-  </main>
-  </>
+    <>
+      <Count />
+    </>
   );
 }
 
 export default App;
- 
