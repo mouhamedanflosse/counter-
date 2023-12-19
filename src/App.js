92da16a8a6a726eff4417afa9c78c841d8714b97
mouhamed anflouss <mouhamedanflosse@gmail.com>
2023-12-19 17:12:59 +0100

    enhancement

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
   <>
   <main className='flex flex-col item-center space-y-2 container mt-28 mx-auto w-60'>
-   <div className={`text-5xl font-bold text-center ${counter > 0 ? "text-blue-500" : counter < 0 && "text-red-500" }`}>{counter}</div>
-   <div className='flex item-center justify-center gap-4'>
+   <p className={`text-5xl font-bold w-full text-center ${counter > 0 ? "text-blue-500" : counter < 0 && "text-red-500" }`}>{counter}</p>
+   <div className='flex item-center w-full justify-center gap-4'>
    <button onClick={() => setCounter((prevState) => prevState + 1 )} class="text-white  shadow-md transition-all bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">+</button>
    <button onClick={() => setCounter((prevState) => prevState - 1 )} class="text-white bg-red-500 hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5">-</button>
    </div>
